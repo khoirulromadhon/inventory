b821d87ed0824673b6347bd85b092dcc5448a8e0
khoirulromadhon <khoirulromadhon1112@gmail.com>
2022-08-05 10:59:01 +0700

    navbar

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("/lihat", views.lihat, name="lihat"),
-    path("/about", views.about, name="about"),
+    path("lihat", views.lihat, name="lihat"),
+    path("about", views.about, name="about"),
 ]
